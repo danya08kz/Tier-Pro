599c0d6ee4a0ac74935174a7e79dc44934556da4
Alexin Danila <danya09kz@gmail.com>
2021-02-24 09:00:16 +0600

    UPD Finish version 2.0

--- index.php
+++
@@ -1,5 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetPageProperty("viewed_show", "Y");
-$APPLICATION->SetTitle("TirePro");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
